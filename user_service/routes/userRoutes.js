7e4e14eec3a7432ffe15017ee135f277d1946c9a
kontrapunkt <michael.varga@23degrees.io>
2023-04-21 14:14:49 +0200

    auth route finished

--- user_service/routes/userRoutes.js
+++ user_service/routes/userRoutes.js
@@ -39,6 +39,34 @@ router.post("/sign-up", async (req, res) => {
   });
 
 // Auth route
-router.post("/auth", async (req, res) => {});
+router.post("/auth", async (req, res) => {
+    try {
+      const { userName, password } = req.body;
+  
+      if (!userName || !password) {
+        return res.status(400).json({ message: "Username and password are required." });
+      }
+  
+      const user = await User.findOne({ userName });
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found." });
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid password." });
+      }
+  
+      const token = jwt.sign({ userId: user._id, userSlug: user.userSlug }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+  
+      res.status(200).json({ message: "Authentication successful.", token });
+    } catch (error) {
+      res.status(500).json({ message: "Error during authentication.", error });
+    }
+  });
 
 module.exports = router;
