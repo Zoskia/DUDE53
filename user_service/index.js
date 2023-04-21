2c7bc9594b218313af30e8f2c957928cb2fce0ee
kontrapunkt <michael.varga@23degrees.io>
2023-04-21 16:42:40 +0200

    user service finished. no gateway redirekt?!

---
+++ user_service/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+
+// Connect to MongoDB database
+const MONGODB_URI = "mongodb://localhost:27017/user_service";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB database");
+});
+
+// Create Express application
+const app = express();
+
+// Add middleware to parse JSON data and handle CORS errors
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+// Add created routes
+app.use("/user", userRoutes);
+
+// Start the server
+const port = 3001;
+app.listen(port, () => {
+  console.log(`User Service listening on port ${port}`);
+});
