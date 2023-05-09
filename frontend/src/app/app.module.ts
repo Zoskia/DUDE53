7805a3b20e83674eb9b24a383532a651b4451e34
kontrapunkt <michael.varga@23degrees.io>
2023-05-09 13:10:07 +0200

    basic nav with pico

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './core/components/navigation/navigation.component';
+
+import { RouterModule } from '@angular/router';
+import { AuthComponent } from './core/components/auth/auth.component';
+import { SignInComponent } from './core/components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'auth', component: AuthComponent},
+      {path: 'sign-in', component: SignInComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
